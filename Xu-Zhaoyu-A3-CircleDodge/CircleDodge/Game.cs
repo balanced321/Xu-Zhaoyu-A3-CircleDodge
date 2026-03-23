915c5220df677787552c0af6246dd916d86cf892
balanced321 <zhaoyu.xu@mohawkcollege.ca>
2026-03-23 05:24:29 -0400

    Add collectible star, scoring, and win lose states

--- Xu-Zhaoyu-A3-CircleDodge/CircleDodge/Game.cs
+++ Xu-Zhaoyu-A3-CircleDodge/CircleDodge/Game.cs
@@ -7,6 +7,12 @@ public class Game
 {
     Player player;
     FallingEnemy[] enemies;
+    StarCollectible star;
+
+    bool gameOver;
+    bool gameWin;
+    int score;
+    int targetScore = 5;
 
     public void Setup()
     {
@@ -15,6 +21,32 @@ public class Game
         Window.TargetFPS = 60;
         Window.CentreWindow();
 
+        ResetGame();
+    }
+
+    public void Update()
+    {
+        Window.ClearBackground(Color.OffWhite);
+
+        if (!gameOver && !gameWin)
+        {
+            UpdateGame();
+        }
+        else
+        {
+            if (Input.IsKeyboardKeyPressed(KeyboardInput.Space))
+            {
+                ResetGame();
+            }
+        }
+
+        DrawBackground();
+        DrawGame();
+        DrawUI();
+    }
+
+    void ResetGame()
+    {
         player = new Player(400, 520, 20, 5f);
 
         enemies = new FallingEnemy[10];
@@ -27,25 +59,80 @@ public class Game
 
             enemies[i] = new FallingEnemy(x, y, radius, speed);
         }
+
+        star = new StarCollectible(400, 200, 12);
+        star.Respawn();
+
+        score = 0;
+        gameOver = false;
+        gameWin = false;
     }
 
-    public void Update()
+    void UpdateGame()
     {
-        Window.ClearBackground(Color.OffWhite);
-
         player.Update();
 
         for (int i = 0; i < enemies.Length; i++)
         {
             enemies[i].Update();
+
+            if (player.CollidesWith(enemies[i].Position, enemies[i].Radius))
+            {
+                gameOver = true;
+            }
         }
 
-        DrawBackground();
+        if (player.CollidesWith(star.Position, star.Radius))
+        {
+            score++;
+            star.Respawn();
+
+            if (score >= targetScore)
+            {
+                gameWin = true;
+            }
+        }
+    }
+
+    void DrawGame()
+    {
         player.DrawSelf();
 
         for (int i = 0; i < enemies.Length; i++)
         {
             enemies[i].DrawSelf();
+        }
+
+        star.DrawSelf();
+    }
+
+    void DrawUI()
+    {
+        Text.Color = Color.Black;
+        Text.Size = 24;
+        Text.Draw("Score: " + score, 20, 20);
+        Text.Draw("Goal: " + targetScore, 20, 50);
+
+        if (gameOver)
+        {
+            Text.Color = Color.Red;
+            Text.Size = 40;
+            Text.Draw("GAME OVER", 270, 250);
+
+            Text.Color = Color.Black;
+            Text.Size = 22;
+            Text.Draw("Press SPACE to restart", 285, 300);
+        }
+
+        if (gameWin)
+        {
+            Text.Color = Color.Green;
+            Text.Size = 40;
+            Text.Draw("YOU WIN!", 295, 250);
+
+            Text.Color = Color.Black;
+            Text.Size = 22;
+            Text.Draw("Press SPACE to restart", 285, 300);
         }
     }
 
---
+++ Xu-Zhaoyu-A3-CircleDodge/CircleDodge/StarCollectible.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace MohawkGame2D;
+
+public class StarCollectible
+{
+    public Vector2 Position;
+    public float Radius;
+
+    public StarCollectible(float x, float y, float radius)
+    {
+        Position = new Vector2(x, y);
+        Radius = radius;
+    }
+
+    public void Respawn()
+    {
+        Position.X = Random.Integer((int)Radius + 20, Window.Width - (int)Radius - 20);
+        Position.Y = Random.Integer(80, 500);
+    }
+
+    public void DrawSelf()
+    {
+        Draw.FillColor = Color.Yellow;
+        Draw.LineColor = Color.Black;
+        Draw.LineSize = 2;
+        Draw.Circle(Position, Radius);
+    }
+}
