25da8ce47e08038225f65cc44fd58d2e6a4aca8f
balanced321 <zhaoyu.xu@mohawkcollege.ca>
2026-03-23 01:32:23 -0400

    Create game window and draw basic player

---
+++ Xu-Zhaoyu-A3-CircleDodge/CircleDodge/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace MohawkGame2D;
+
+public class Game
+{
+    Player player;
+
+    public void Setup()
+    {
+        Window.SetSize(800, 600);
+        Window.SetTitle("Circle Dodge");
+        Window.TargetFPS = 60;
+        Window.CentreWindow();
+
+        player = new Player(400, 520, 20, 5f);
+    }
+
+    public void Update()
+    {
+        Window.ClearBackground(Color.OffWhite);
+
+        DrawBackground();
+        player.DrawSelf();
+    }
+
+    void DrawBackground()
+    {
+        Draw.LineColor = Color.DarkGray;
+        Draw.LineSize = 4;
+        Draw.Line(0, 560, 800, 560);
+    }
+}
---
+++ Xu-Zhaoyu-A3-CircleDodge/CircleDodge/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace MohawkGame2D;
+
+public class Player
+{
+    public Vector2 Position;
+    public float Radius;
+    public float Speed;
+
+    public Player(float x, float y, float radius, float speed)
+    {
+        Position = new Vector2(x, y);
+        Radius = radius;
+        Speed = speed;
+    }
+
+    public void DrawSelf()
+    {
+        Draw.FillColor = Color.Blue;
+        Draw.LineColor = Color.Black;
+        Draw.LineSize = 2;
+        Draw.Circle(Position, Radius);
+    }
+}
